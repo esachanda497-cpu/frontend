4278c3d7c45060e914e1db3a4d628cf7b41a302e
Esa Chanda <esachanda497@gmail.com>
2026-09-17 15:44:31 +0530

    webcam test script using OpenCV

---
+++ webcamtest.py
@@ -0,0 +1,30 @@
+import cv2
+
+
+cap = cv2.VideoCapture(0)
+
+while True:
+
+    ret, frame = cap.read()
+
+    if not ret:
+
+        print("Could not access webcam.")
+
+        break
+
+    cv2.imshow(
+        "SafeStep Webcam Test",
+        frame
+    )
+
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+
+        break
+
+
+cap.release()
+
+cv2.destroyAllWindows()
